8e87304501dfd962c7e4a0665030adbe9c2160e7
kaiky-lima31 <kaiky.lima357@gmail.com>
2023-05-02 14:45:30 -0300

    sintaxe geral

---
+++ 01-sintaxe-geral.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sintaxe geral do PHP</title>
+</head>
+<body>
+<h1>Experimento PHP</h1>
+<hr>
+
+<?php
+echo "Olá PHP!";
+echo "teste";
+
+$escola = "Senac";
+
+echo $escola;
+
+// Comentários de um linha CTRL
+
+/* ALT SHIFT A 
+Comentários de mútiplas linhas */
+
+?>
+
+
+</body>
+</html>
